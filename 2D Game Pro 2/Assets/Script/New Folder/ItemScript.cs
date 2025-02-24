506addcbd18d2568178968e292f740e254d2960d
TommieSr20 <143731792+TommieSr20@users.noreply.github.com>
2025-02-24 11:38:55 -0500

    commit

--- 2D Game Pro 2/Assets/Script/New Folder/ItemScript.cs
+++ 2D Game Pro 2/Assets/Script/New Folder/ItemScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemNameList : MonoBehaviour
+public class ItemScript : MonoBehaviour
 {
 
     public string Key;
